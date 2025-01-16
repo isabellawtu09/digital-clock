36ea0a780049bfbe85eaab9c30e70b752a72bb39
isabellawtu09 <141868250+isabellawtu09@users.noreply.github.com>
2025-01-15 19:52:54 -0600

    Added digital clock component and a background image behind the clock counter

---
+++ clock/src/Clock.jsx
@@ -0,0 +1,55 @@
+
+import React, {useState, useEffect} from 'react';
+
+
+function Clock(){
+
+const [time , setTime ] = useState(new Date());
+
+//once component is mounted 
+useEffect(() => {
+    const intervalId = setInterval(() =>  {
+        setTime(new Date());
+    }, 1000);
+
+// when unmounting component => cleanup
+    return () => {
+        clearInterval(intervalId);
+    }
+
+} , []);
+
+
+function formatTime(){
+
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridian = hours >= 12 ? "pm" : "am";
+    // if hour == 12 return 12 not  0
+    hours = hours % 12|| 12;
+
+return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+
+function padZero(number){
+    return (number < 10 ? "0"  : "" )+ number;
+}
+
+return(
+
+<div className = "clock-container">
+
+    <div className = "clock">
+        <span>{formatTime()}</span>
+    </div>
+
+</div>
+
+);
+
+
+}
+
+export default Clock
